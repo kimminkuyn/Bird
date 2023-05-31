8277fc5b3a92bc8a2fcdc2c8fb3fd00c2a3a08a1
User <alsrbs49@naver.com>
2023-05-31 17:02:05 +0900

    중간 완성

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Appbar',
+      title: '백과 새전',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     ),
   );
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -34,66 +34,146 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
+
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset('images/swan.jpg',
+            width:  250,
+            height: 250,),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.green;
+                    }
+                  },
+                ),
+                foregroundColor: MaterialStateProperty.resolveWith(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.black;
+                    }
+                  },
+                ),
+              ),
+              child: Text('다음'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.green;
+                    }
+                  },
+                ),
+                foregroundColor: MaterialStateProperty.resolveWith(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.black;
+                    }
+                  },
+                ),
+              ),
+              child: Text('뒤로'),
+            ),
+            TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.green;
+                    }
+                  },
+                ),
+                foregroundColor: MaterialStateProperty.resolveWith(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.black;
+                    }
+                  },
+                ),
+              ),
+              child: Text('처음으로'),
+            ),
+          ],
+        ),
+      ),
       drawer: Drawer(
         child: ListView(
           children: [
             UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('images/kar.jpg'),
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: AssetImage('images/yuri.jpg'),
               ),
-              accountName: Text('김민균'),
-              accountEmail: Text('alsrbs49@naver.com'),
+              otherAccountsPictures: const [
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/hon.jpg'),
+                ),
+              ],
+              accountName: const Text('김민균'),
+              accountEmail: const Text('alsrbs49@naver.com'),
+              onDetailsPressed: () {},
+              decoration: BoxDecoration(
+                color: Colors.lightBlueAccent[200],
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(10.0),
+                  bottomRight: Radius.circular(10.0),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              iconColor: Colors.lightBlueAccent,
+              focusColor: Colors.lightBlueAccent,
+              title: const Text('홈'),
+              onTap: () {},
+              trailing: const Icon(Icons.navigate_next),
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              iconColor: Colors.lightBlueAccent,
+              focusColor: Colors.lightBlueAccent,
+              title: const Text('봄'),
+              onTap: () {},
+              trailing: const Icon(Icons.navigate_next),
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              iconColor: Colors.lightBlueAccent,
+              focusColor: Colors.lightBlueAccent,
+              title: const Text('여름'),
+              onTap: () {},
+              trailing: const Icon(Icons.navigate_next),
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              iconColor: Colors.lightBlueAccent,
+              focusColor: Colors.lightBlueAccent,
+              title: const Text('가을'),
+              onTap: () {},
+              trailing: const Icon(Icons.navigate_next),
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              iconColor: Colors.lightBlueAccent,
+              focusColor: Colors.lightBlueAccent,
+              title: const Text('겨울'),
+              onTap: () {},
+              trailing: const Icon(Icons.navigate_next),
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required Key? key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
